8aea5bd4973cbd68977fac90e2e393bf758b232c
oatbestzaza <oatbestzaza@gmail.com>
2022-06-30 14:19:44 +0700

    test

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-#Author: Chatchawal Sangkeettrakarn
-#Date: September 20,2020.
+from typing import Union
 
 from fastapi import FastAPI
-import uvicorn
-from typing import Union
+
 app = FastAPI()
 
+
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
+
 @app.get("/items/{item_id}")
-async def read_item(item_id: int, q: Union[str, None] = None):
+def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
