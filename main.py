addf9f3e57a5418c4058f1c0eb866a3602e26a9b
oatbestzaza <oatbestzaza@gmail.com>
2022-06-30 14:58:39 +0700

    test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+
 @app.get("/")
 async def main():
     return 'Hello World'
@@ -7,17 +8,3 @@ async def main():
 @app.get("/test")
 async def test():
     return 'Test Tutorial'
-
-@app.get("/add")
-async def add(a: int = 0, b: int = 0):
-    return a+b
-
-@app.get("/mul")
-async def mul(a: int = 0, b: int = 0):
-    return a*b
-
-@app.get("/pow")
-async def pow(a: int = 0, b: int = 0):
-    return math.pow(a,b)
-if __name__ == '__main__':
-   uvicorn.run(app, host="0.0.0.0", port=80, debug=True) 
