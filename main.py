3b4a43a8227405c4ff940dddac4694cb273cfa28
oatbestzaza <oatbestzaza@gmail.com>
2022-06-30 15:01:18 +0700

    test

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
 import uvicorn
 
-@app.get("/")
-async def main():
-    return 'Hello World'
-
-@app.get("/test")
-async def test():
-    return 'Test Tutorial'
+@app.get("/ngrams")
+def bigrams(text):
+    #text = 'chatchawal 1234'
+    num = 2
+    #print(len(text))
+    sto = []
+    #def ngrams(text,num):
+    for x in range(len(text)-(num-1)):
+        print(x,text[x:x+num])
+        sto.append(text[x:x+num])
+    #ngrams(text,num)
+    return sto
