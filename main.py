cb10393fede99c434090e77012e2ef6d8b9a7193
Limited <67133068+oatbestzaza@users.noreply.github.com>
2022-07-01 15:59:56 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from fastapi.responses import PlainTextResponse
 import fasttext 
 import fasttext.util
 
-
+@app.get("/model")
 async def modeloutput(txt):
     model = fasttext.load_model('r_model_50dgamestatus.bin')
     return {model.predict(txt, k=5, threshold=0.01)}
